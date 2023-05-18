62547a576e50025a1879981fa86b25293bb3ac34
Mason <mashow15@yahoo.com>
2023-05-17 23:14:52 -0500

    route initialize

---
+++ flask_app/controllers/dashboard.py
@@ -0,0 +1,17 @@
+from flask_app import app
+from flask import render_template, redirect, request, session
+from flask_app.models.user import User
+from flask_app.models.blog import Blog
+from flask_app.models.like import Like
+
+
+@app.route('/home')
+def home_page():
+    if 'user_id' not in session:
+        return redirect('/login')
+    data = {
+        'id': session['user_id']
+    }
+    user = User.get_with_id(data)
+    blogs = Blog.all_blogs()
+    return render_template('home.html', user = user, blogs = blogs)
